bdc76b6b28b215590e2bf61b5841262bfd3ac6fb
stephenbuys404 <114079563+stephenbuys404@users.noreply.github.com>
2023-10-14 20:05:38 +0200

    Delete findservices.cmd

--- findservices.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal EnableDelayedExpansion
-sc \\localhost query | findstr SERVICE_NAME > services.lst
-for /f "tokens=1,2" %%A in (services.lst) do (
-    echo %%B
-    sc \\localhost qc %%B | findstr SERVICE_START_NAME
-)
-del services.lst
-pause()
